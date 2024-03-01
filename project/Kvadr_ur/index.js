56eca4aab40743d4d673a16cd52be18bf6449908
KrisSorIs <148820684+KrisSorIs@users.noreply.github.com>
2024-03-01 12:11:32 +0300

    квадратные уравнения не алерт, а иннер хтмл

--- project/Kvadr_ur/index.js
+++ project/Kvadr_ur/index.js
@@ -3,7 +3,7 @@ function onSubmit() {
     let input2 = Number(document.getElementById('input2').value);
     let input3 = Number(document.getElementById('input3').value); 
     if(input1 == 0 && input2 == 0 && input3 == 0){
-         alert('Корнями уравнения являются все действительные числа');
+        document.getElementById("korni").innerHTML = "Корнями уравнения являются все действительные числа."//alert('Корнями уравнения являются все действительные числа');
          return;
     }
     //else if(input1 >= -10000 && input2 >= -10000 && input3 >= -10000)
@@ -12,9 +12,20 @@ function onSubmit() {
         let x1 = (-1 * input2 - Math.sqrt(D))/ 2 * input1;
         let x2 = (-1 * input2 + Math.sqrt(D))/ 2 * input1;
         let x = -1 * input2 / 2 * input1;
-        D > 0 ? alert(`Уровнение имеет два корня \n x1 = ${x1.toFixed(2)} \n x2 = ${x2.toFixed(2)}`) : 
-        (D == 0 ? alert(`Уровнение имеет один действительный корень \n x = ${x.toFixed(2)}`) : 
-        (D < 0 ? alert('Уровнение не имеет корней'): alert('')));
+        if(D > 0){
+            document.getElementById("korni").innerHTML = "Корни уравнения: "
+            document.getElementById("X1").innerHTML = "X1 = " + x1.toFixed(2);
+            document.getElementById("X2").innerHTML = "X2 = " + x2.toFixed(2);
+        } 
+        if (D==0){
+            document.getElementById(korni).innerHTML = "Корень уравнения: ";    
+            document.getElementById("X1").innerHTML = "X = " + x1.toFixed(2);
+        }//? alert(`Уровнение имеет два корня \n x1 = ${x1.toFixed(2)} \n x2 = ${x2.toFixed(2)}`) : 
+        //(D == 0 ? alert(`Уровнение имеет один действительный корень \n x = ${x.toFixed(2)}`) : 
+        //(D < 0 ? document.getElementById(korni).innerHTML = "Уравнение не имеет корней."//alert('Уровнение не имеет корней'): alert('')));
+        if (D<0){
+            document.getElementById("korni").innerHTML = "Уравнение не имеет корней."
+        }
     }
     //else{
     //    alert('Данные введены некорректно');
@@ -24,4 +35,5 @@ function onClear() {
     let input1 = Number(document.getElementById('input1').value = '');
     let input2 = Number(document.getElementById('input2').value = '');
     let input3 = Number(document.getElementById('input3').value = ''); 
+    document.getElementById("korni").innerHTML = "";
 }
